24b6c54daf825bc209f6f10f4fcdc12ab0341425
Felix <162381046+GrandTheftTag@users.noreply.github.com>
2025-09-19 14:53:53 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// KORREKTUR: 'App' wird jetzt als Standard-Export ohne geschweifte Klammern importiert.
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
